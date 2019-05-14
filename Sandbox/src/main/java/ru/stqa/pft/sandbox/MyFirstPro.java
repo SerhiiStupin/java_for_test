dab54079377542f4a815a9baa4ca5c3b4feea360
StiggUA <45452395+StiggUA@users.noreply.github.com>
2019-05-14 21:00:04 +0300

    Добавлено несколько функций

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstPro.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstPro.java
@@ -2,6 +2,26 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstPro {
   public static void main(String[] args) {
-    System.out.println("Hello, world");
+    hello("world");
+    hello("user");
+    hello("Sergey");
+
+    double l = 5;
+    System.out.println("Площадь квадарата со сторой " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со свторнами " + a + " и " + b + " = " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l) {
+    return l * l;
+  }
+  public static double area(double a, double b) {
+    return a * b;
   }
 }
