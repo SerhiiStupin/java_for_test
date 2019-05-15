507d20aae25df0ab040c6dbf165808ae3b3f84eb
StiggUA <45452395+StiggUA@users.noreply.github.com>
2019-05-15 07:48:53 +0300

    New Functions

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstPro.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstPro.java
@@ -6,22 +6,17 @@ public class MyFirstPro {
     hello("user");
     hello("Sergey");
 
-    double l = 5;
-    System.out.println("Площадь квадарата со сторой " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадарата со сторой " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со свторнами " + a + " и " + b + " = " + area(a, b));
+    Rectangle r = new Rectangle(4, 6);
+
+    System.out.println("Площадь прямоугольника со свторнами " + r.a + " и " + r.b + " = " + r.area());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(double l) {
-    return l * l;
-  }
-  public static double area(double a, double b) {
-    return a * b;
-  }
+
 }
---
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+
+
+  public static double distance(Point p1, Point p2);
+            return p2 - p1;
+
+  {
+    System.out.println("Расстояние составляет" + (Point p2 - Point p1));
+  }
+}
